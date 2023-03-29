79e678cf37cf09a2dd440719dcee94a5701f6687
Sarah Kuss <sarah.kuss33@gmail.com>
2023-03-29 15:50:16 -0400

    added CRUM

--- index.js
+++ index.js
@@ -14,15 +14,47 @@ initializeApp({
 const db = getFirestore();
 
 // Add a product to our products collection (a new document)
-const candy = {
-  name: "Skittles",
-  unitPrice: 3.99,
-  size: "16 oz",
-  color: "green",
-  inventory: 144,
-  productNumber: 7,
+const candy2 = {
+  name: "Twix",
+  unitPrice: 2.99,
+  size: "12 oz",
+  color: "gold",
+  inventory: 288,
+  productNumber: 2,
 }
 
-db.collection('products').add(candy)
-  .then(doc => console.log("added doc: " + doc.id))
-  .catch(err => console.log(err))
+// How to add a document to Firestore:
+
+// db.collection('products').add(candy2) // Returns a promise not a document. While we are waiting for the promise...
+//   .then((doc) => {
+//     console.log("added doc: " + doc.id)
+//   // I can be sure .then() that the first process was completed successfully
+//   })
+//   .catch(err => console.log(err))
+
+// How to update a document
+db.collection('products').doc('RHCCn8BaG7ELsVPq7qml').update({
+  inventory: 555,        // updates
+  customerFavorite: true // adds to document
+})
+
+// How to read a document from Firestore:
+
+db.collection('products').doc('RHCCn8BaG7ELsVPq7qml').get()
+  .then(doc => {            // don't need () around doc bc only 1 parameter
+    console.log(doc.data());
+  })
+  .catch(err => console.log(err));
+  // .catch(console.log) *can also write it this way
+
+  // How to get a whole collection:
+
+  db.collection('products').get()
+    .then(collection => {
+      const productList = collection.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+      console.table(productList);
+    })
+    .catch(console.log);
+
+  //To delete:
+  // db.collection('products').doc('RHCCn8BaG7ELsVPq7qml').delete()
